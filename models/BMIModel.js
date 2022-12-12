1b2ff2a6f2c5f1274a140eecd931edea16e552e5
Lakhan9525 <lakhanraj987@gmail.com>
2022-12-12 19:01:02 +0530

    raja

--- models/BMIModel.js
+++ models/BMIModel.js
@@ -1,8 +1,11 @@
 const mongoose= require("mongoose");
 
 const bmiSchema= new mongoose.Schema({
+
     BMI:{type:Number,required:true},
+
     height:{type:String,required:true},
+    
     weight:{type:String,required:true},
     user_id:{type:String,required:true},
 },{
